dcf35a287d9e8272a2e496e772de80ff399f9df7
valteir costa de sousa <valteircosta@gmail.com>
2021-05-01 14:34:10 +0000

    Add job_test

--- encoder/domain/job.go
+++ encoder/domain/job.go
@@ -1,13 +1,59 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+//First method executed
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
 type Job struct {
-	ID               string
-	OutputBucketPath string
-	Status           string
-	Video            *Video
-	Error            string
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
+	ID               string    `valid:"uuid"`
+	OutputBucketPath string    `valid:"notnull"`
+	Status           string    `valid:"notnull"`
+	Video            *Video    `valid:"-"`
+	Error            string    `valid:"-"`
+	CreatedAt        time.Time `valid:"-"`
+	UpdatedAt        time.Time `valid:"-"`
+}
+
+//Função que funciona como um construtor retornado dois valores um job ou um errror
+func NewJob(output string, status string, video *Video) (*Job, error) {
+	//tipo construtor com parametros
+	job := Job{
+		OutputBucketPath: output,
+		Status:           status,
+		Video:            video,
+	}
+	//Roda o prepare preenchendo os campos
+	job.prepare()
+	err := job.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &job, nil
+
+}
+
+//Inicial minúscula visivel dentro packge, no caso domain
+func (job *Job) prepare() {
+	job.ID = uuid.NewV4().String()
+	job.CreatedAt = time.Now()
+	job.UpdatedAt = time.Now()
+}
+
+// (job *Job) Transforma a função em um methodo ligado a Job
+func (job *Job) Validate() error {
+	_, err := govalidator.ValidateStruct(job)
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
